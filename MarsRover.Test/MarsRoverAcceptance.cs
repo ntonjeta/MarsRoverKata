16679feeb11c5417eb855316be806587d9551f85
ntonjeta <ntonjeta@autistici.org>
2020-06-28 22:46:52 +0200

    Add acceptance test class.

--- MarsRover.Test/MarsRoverAcceptance.cs
+++ MarsRover.Test/MarsRoverAcceptance.cs
@@ -1,18 +1,34 @@
 using NUnit.Framework;
+using MarsRover;
 
 namespace MarsRoverAcceptance
 {
     public class Tests
     {
+        private Rover _sut;
+
         [SetUp]
         public void Setup()
         {
+            _sut = new Rover("5 5");
         }
 
         [Test]
-        public void Test1()
+        public void FirstRover()
         {
-            Assert.Pass();
+            _sut.initialPosition = "1 2 N";
+            _sut.moves("LMLMLMLMM");
+
+            Assert.AreEqual(_sut.getPosition(), "1 3 N");
+        }
+
+        [Test]
+        public void SecondRover()
+        {
+            _sut.initialPosition = "3 3 E";
+            _sut.moves("MMRMMRMRRM");
+
+            Assert.AreEqual(_sut.getPosition(), "1 3 N");
         }
     }
 }

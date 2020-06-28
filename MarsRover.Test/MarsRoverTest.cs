4883e615d2f5f3b2f9af8b96009227f969a505de
ntonjeta <ntonjeta@autistici.org>
2020-06-28 22:51:58 +0200

    Adde tringulation.

--- MarsRover.Test/MarsRoverTest.cs
+++ MarsRover.Test/MarsRoverTest.cs
@@ -4,21 +4,37 @@ namespace MarsRover.Test
 {
     public class Tests
     {
-        public static int[,] InitialPlatoue = {
-            {0,0,0,0,0},
-            {0,0,0,0,0},
-            {0,0,0,0,0},
-            {0,0,0,0,0},
-            {0,0,0,0,0}
-        };
 
         private Rover _sut;
 
         [Test]
-        public void ShouldCreateRoverPlatoue()
+        public void ShouldCreateRoverFiveSquarePlatoue()
         {
+            int[,] ExpectedPlateau = {
+                {0,0,0,0,0},
+                {0,0,0,0,0},
+                {0,0,0,0,0},
+                {0,0,0,0,0},
+                {0,0,0,0,0}
+            };
+
             _sut = new Rover("5 5");
-            Assert.AreEqual(InitialPlatoue,_sut.plateau);
+
+            Assert.AreEqual(ExpectedPlateau, _sut.plateau);
+        }
+
+        [Test]
+        public void ShouldCreateRoverSpecifiedDimension()
+        {
+            int[,] ExpectedPlateau = {
+                {0,0},
+                {0,0},
+                {0,0}
+            };
+
+            _sut = new Rover("3 2");
+
+            Assert.AreEqual(ExpectedPlateau, _sut.plateau);
         }
     }
 }

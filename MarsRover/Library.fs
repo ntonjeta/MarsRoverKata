d05fa8136f07fbc7144662e56bf1f1962fce5a96
ntonjeta <ntonjeta@autistici.org>
2020-11-04 18:04:42 +0100

    add implementation

--- MarsRover/Library.fs
+++ MarsRover/Library.fs
@@ -1,54 +1,74 @@
 ﻿namespace MarsRover
 
+// array2D
 type Board = int [,]
 
+// enumeration
 type Orientation =
     | N = 0
     | E = 1
     | S = 2
     | O = 3
 
-type Position = { X: int; Y: int; O: Orientation }
-
+// discriminated union
 type Move =
     | L
     | R
     | M
 
-type RunningRover = { Board: Board; Rover: Position }
+// record
+type Rover = { X: int; Y: int; Or: Orientation }
+
+// record
+type RunningRover = { Board: Board; Rover: Rover }
+
+module Rover =
+    let toLeft actual: Rover =
+        { actual with
+              Or = enum ((int actual.Or + 3) % 4) }
+
+    let toRigth actual: Rover =
+        { actual with
+              Or = enum ((int actual.Or + 1) % 4) }
+
+    let move (actual: Rover) =
+        match actual.Or with
+        | Orientation.N -> { actual with X = actual.X + 1 }
+        | Orientation.S -> { actual with X = actual.X - 1 }
+        | Orientation.E -> { actual with Y = actual.Y + 1 }
+        | Orientation.O -> { actual with Y = actual.Y - 1 }
+        | _ -> actual
 
 module Board =
-    let initialize (x: int) (y: int) (p: Position) =
-        let initBoard = Array2D.init x y (putRover)
-        let putRover x y p  = 
-            for i in x do 
-                for j in y do 
-                    initBoard.[i = p.X, j = p.Y] <- 1
+    let rows (b: Board) = (Array2D.length1 b)
+    let cols (b: Board) = (Array2D.length2 b)
 
-        putRover
+    let create (rows: int) (cols: int) (p: Rover) =
+        Array2D.init<int> rows cols (fun i j -> if i = p.X && j = p.Y then 1 else 0)
 
+    let update (r: RunningRover) =
+        let np = (Rover.move r.Rover)
 
-    let updateBoard (r: RunningRover) (m: Move) = r.Board
+        let outOfTheBoard =
+            np.X > (rows r.Board) || np.Y > (cols r.Board)
 
-module Position =
-    let toLeft r: Position = { r with O = Orientation.N } //TODO
-    let toRigth r: Position = { r with O = Orientation.N } //TODO
-    let move (r: RunningRover) (m: Move) = r.Rover
-
+        if outOfTheBoard then r.Board else create (rows r.Board) (cols r.Board) np
 
 module MarsRover =
-    let startMarsRover (x: int) (y: int) (p: Position) =
-        { Board = Board.initialize x y p
+    let startRover (x: int) (y: int) (p: Rover) =
+        { Board = Board.create x y p
           Rover = p }
 
     let moveRover (r: RunningRover) (m: Move) =
         match m with
         | M ->
-            { Board = Board.updateBoard r m
-              Rover = Position.move r m }
+            { Board = Board.update r
+              Rover = Rover.move r.Rover }
         | R ->
             { r with
-                  Rover = Position.toRigth r.Rover }
+                  Rover = Rover.toRigth r.Rover }
         | L ->
             { r with
-                  Rover = Position.toLeft r.Rover }
+                  Rover = Rover.toLeft r.Rover }
+
+    let print (r: RunningRover) = printfn "%s" (r.Board.ToString())
